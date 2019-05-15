edf1aacddaa4a439f638558c68c0ab77e0cf3f6a
LeaKiller <lea.keller.opp@gmail.com>
2019-05-15 09:16:11 +0200

    Tentative de récupération

--- projet_pantenomos/client/main.js
+++ projet_pantenomos/client/main.js
@@ -6,12 +6,14 @@ import '../imports/templates/header.html';
 import '../imports/templates/homePage.html';
 import '../imports/templates/hp_copyright.html';
 import '../imports/templates/hp_project.html';
+import '../imports/templates/loginButton.html';
+import '../imports/templates/navbar.html';
 import '../imports/templates/newProject.html';
+import '../imports/templates/sidebar.html';
 import '../imports/templates/vot_firstPart.html';
 import '../imports/templates/vot_secondPart.html';
 import '../imports/templates/vot_thirdPart.html';
 import '../imports/templates/votation.html';
-import '../imports/templates/navbar.html';
 
 
 //importation de fichiers JavaScript
