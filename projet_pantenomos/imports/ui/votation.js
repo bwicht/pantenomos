15cef9d4a37112a674849687a0c308ec22efaadb
LeaKiller <lea.keller.opp@gmail.com>
2019-05-18 17:32:34 +0200

    Tentative de code de surlignage

--- projet_pantenomos/imports/ui/votation.js
+++ projet_pantenomos/imports/ui/votation.js
@@ -10,4 +10,34 @@ Template.vot_secondPart.helpers({
     },
 });
 
-//surligner et commenter les amendements
+// //surligner et commenter les amendements
+// Template.vot_secondPart.helpers({
+
+//     laws: function getSelectedText() {
+
+//         t = (document.all) ? document.selection.createRange().text : document.getSelection();
+    
+//         return t;
+//     },
+// });
+
+// Template.vot_secondPart.helpers({
+
+// laws: function highlightText() {
+    
+//     var selection = getSelectedText();
+//     var selection_text = selection.toString();
+
+//     // How do I add a span around the selected text?
+
+//     var highlight = document.createElement('mark');
+//     highlight.textContent = selection_text;
+
+//     var range = selection.getRangeAt(0);
+//     range.deleteContents();
+//     range.insertNode(highlight);
+
+//     },
+// });
+
+// document.getElementById("laws").addEventListener("mouseup", highlightText);
