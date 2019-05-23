fc275c5d0420f46415d99952fc0a2c0733294e1d
LeaKiller <lea.keller.opp@gmail.com>
2019-05-23 11:41:30 +0200

    Update lawText.js

--- projet_pantenomos/imports/ui/lawText.js
+++ projet_pantenomos/imports/ui/lawText.js
@@ -2,64 +2,99 @@
 import { Projects } from '../api/db_projects.js';
 import '../templates/vot_lawText.html';
 
-let firstClickDiv;
+function findSubArticle(el) {
+
+    while (el.parentNode) {
+
+        el = el.parentNode;
+
+        if (el.className === "subArticle") {
+
+            return el;
+        }
+    }
+
+    return null;
+}
+
 
 //surlignage en fonction du choix du sondage
 Template.vot_lawText.events({
 
-    'mousedown .subArticle'(event, instance) {
-
-        firstClickDiv = event.target;
-    },
-
     'mouseup .subArticle'(event, instance) {
 
-        if (firstClickDiv == event.target) {
+        let selection = document.getSelection();
 
-            let selection = document.getSelection();
+        let selectionStart = findSubArticle(selection.anchorNode);
 
-            let selection_text = selection.toString();
+        let selectionText = selection.toString();
 
-            let highlight = document.createElement('span');
+        if (selectionText.split('\n').length > 1) {
 
-            let pollChoice = document.querySelector('.custom-control-input:checked').value;
+            selection.collapseToStart();
 
-            if (pollChoice == "Pour") {
+            let index = 0;
 
-                highlight.className = "highlightPour";
+            //Etedons la sélection jusqu'à trouver un retour à la ligne...
+            while (true) {
+
+                if (selection.toString()[index] != "\n") {
+
+                    try {
+                                
+                        selection.extend(selection.anchorNode, index++);
+                    }
+
+                    catch {
+
+                        break;
+                    }
+                }
             }
 
-            else if (pollChoice == "Contre") {
+            selectionText = selection.toString();
 
-                highlight.className = "highlightContre";
-            }
-
-            else if (pollChoice == "PourPrincipe") {
-
-                highlight.className = "highlightPourPrincipe";
-            }
-
-            else if (pollChoice == "ContrePrincipe") {
-
-                highlight.className = "highlightContrePrincipe";
-            }
-
-            selection_text = selection_text.split('\n')[0];
-
-            highlight.textContent = selection_text;
-
-            let range = selection.getRangeAt(0);
-        
-            range.deleteContents();
-
-            range.insertNode(highlight);
         }
+
+        let highlight = document.createElement('span');
+
+        let pollChoice = document.querySelector('.custom-control-input:checked').value;
+
+        if (pollChoice == "Pour") {
+
+            highlight.className = "highlightPour";
+        }
+
+        else if (pollChoice == "Contre") {
+
+            highlight.className = "highlightContre";
+        }
+
+        else if (pollChoice == "PourPrincipe") {
+
+            highlight.className = "highlightPourPrincipe";
+        }
+
+        else if (pollChoice == "ContrePrincipe") {
+
+            highlight.className = "highlightContrePrincipe";
+        }
+
+        selectionText = selectionText.split('\n')[0];
+
+        highlight.textContent = selectionText;
+
+        let range = selection.getRangeAt(0);
+    
+        range.deleteContents();
+
+        range.insertNode(highlight);
     },
 });
 
 //extrait les données collectées dans la BD
 Template.vot_lawText.helpers({
-    
+
     project: function() {
 
         let currentProject = Projects.findOne({_id: FlowRouter.getParam('_id')});
