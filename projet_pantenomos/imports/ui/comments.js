2377aaa2899cd2772a8a6317f55b54743c93c709
VictorVermot <victorvermot@gmail.com>
2019-05-25 15:19:57 +0200

    Test

--- projet_pantenomos/imports/ui/comments.js
+++ projet_pantenomos/imports/ui/comments.js
@@ -4,7 +4,6 @@ import '../templates/vot_comments.html';
 
 // Lignes 6-35 : event et helper pour récupérer le commentaire et l'id de l'utilisateur connecté 
 Template.vot_comments.events({
-
   'submit .js-create-comment'(event,instance) {
 
     //empêche le navigateur par défaut de soumettre le formulaire
@@ -23,42 +22,53 @@ Template.vot_comments.events({
    Comments.insert(commentDoc)
 
    event.target.content.value = "";
-  }
+},  
+  'submit .newComment'(event,instance){
 
+    event.preventDefault();
+
+    let newComment = {
+        articleId: FlowRouter.getParam('articleId'),
+        text: document.getElementById("txtComment").value,
+        pseudo: document.getElementById("pseudoComment").value,
+        createdAt: new Date(),
+  };
+
+  Meteor.call('comments.create', newComment);
+ 
+  document.getElementById("txtComment").value = "";
+  document.getElementById("pseudoComment").value = "";
+
+  }
+/*
+  'submit .newComment'(event, instance) {
+    //empêche le navigateur par défaut de soumettre le formulaire
+    event.preventDefault();
+ 
+    
+
+   Meteor.call('comments.create', newComment);
+ 
+   document.getElementById("txtComment").value = "";
+   document.getElementById("pseudoComment").value = "";
+   }
+   */
 });
 
 Template.comment_list.helpers({
   comments(){
-    return Comments.find({articleId: FlowRouter.getParam('articleId')});
-  }
-
-})
-// Lignes 37-64 : event et helper pour récupérer le commentaire et l'id de l'utilisateur déconnecté 
-Template.vot_comments2.events({
-
-  'submit .newComment'(event) {
-   //empêche le navigateur par défaut de soumettre le formulaire
-   event.preventDefault();
-
-   let newComment = {
-    project_id: FlowRouter.getParam('_id'),
-    text: document.getElementById("txtComment").value,
-    pseudo: document.getElementById("pseudoComment").value,
-    createdAt: new Date(),
-  };
-  Meteor.call('comments.create', newComment);
-
-  document.getElementById("txtComment").value = "";
-  document.getElementById("pseudoComment").value = "";
-  }
+    return Comments.find({articleId: FlowRouter.getParam('articleId')});    
+  },
 });
+// Lignes 37-64 : event et helper pour récupérer le commentaire et l'id de l'utilisateur connecté 
+
 
 //extrait les données collectées dans la BD
-Template.vot_comments2.helpers({
+/*Template.vot_comments2.helpers({
   
   comments: function() {
 
     //A modifier pour ne chercher que les commentaires propres au projet FlowRouter.getParam('_id')
     return Comments.find({"comment.project_id": FlowRouter.getParam('_id')}).fetch();
  },
-});
+});*/
