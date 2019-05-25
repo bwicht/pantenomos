b373ce788231f03c9d2b0a05a6ef69bf16a84833
bwicht <bertil.wicht@unil.ch>
2019-05-25 20:23:35 +0200

    polling
    polling

--- projet_pantenomos/imports/ui/poll.js
+++ projet_pantenomos/imports/ui/poll.js
@@ -1,44 +1,66 @@
 //importation des méthodes
+import { ReactiveVar } from 'meteor/reactive-var';
 import  { Cases } from '../api/db_cases.js';
+// nom de la database à remplir
 import '../templates/vot_poll.html';
 
-//initialisation des variables qui définiront le choix de l'utilisateur
-//+ un compteur tuto
-//Template.vot_poll.onCreated(function vot_pollOnCreated() {
+
+// initialisation des variables qui définiront le choix de l'utilisateur
+Template.vot_poll.onCreated(function () {
     
-//     this.case1 = new ReactiveVar (0);
-//     this.case2 = new ReactiveVar (0);
-//     this.case3 = new ReactiveVar (0);
-//     this.case4 = new ReactiveVar (0);
-//     this.counter = new ReactiveVar (0);
+    Pour =0;
+    Contre =0;
+    PourPrincipe=0;
+    ContrePrincpe=0;
+});
 
-//});
 
-//helpers = fonctions, on leur donne un paramètre et ils rendent un paramètre si nécessaire
 Template.vot_poll.helpers({
-    cases() {
-        return Cases.find().fetch();
+    choix() {
+        return Template.instance().Pour.get()
   
      },
-     choix() {
-        return Template.instance().case1.get();
-     },
+     
   });
 
-//MERCI D'AJOUTER UN COMMENTAIRE
+//
 Template.vot_poll.events({
 
     'checked .poll'(event, instance) {
-        event.target
-        instance.case.set(instance.case1.get() + 1 );
+        event.target;
         votes.update({article: 72},{$inc : {PourPrincipe : 1 }})
     },
 
     'checked .poll'(event, instance){
         Cases.insert({
-            value: instance.case1.get(),
-            createdAt: new Date()
+            vote: choix,
+            vote: document.getElementByName('choice').value,
+            createdAt: new Date(),
+            ownerId: Meteor.userId()
         });
     },
-});
+    
 
+    'click #btnAddVote': function(event,instance) {
+        event.preventDefault();
+        let radio = document.getElementsByName('choice').value;
+        alert(radio);
+        let recuperation = document.getElementByName('choice').value;
+        let recuperation2 = pollChoice.choice.value;
+        alert(recuperation2);
+        
+        
+        // let vote = {
+        //     vote: document.getElementByName('choice').value,
+        //     // ajouter pour quelle initiative il voté ?
+        //     createdAt: new Date(),
+        //     ownerId: Meteor.userId()
+        //     }
+    
+        //     Cases.insert(vote)
+        //     // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
+    
+            
+        },
+
+});
