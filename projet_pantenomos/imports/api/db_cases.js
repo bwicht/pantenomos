d6f80ec78fef9e767450eaca21c70d3c1477f027
bwicht <bertil.wicht@unil.ch>
2019-05-26 17:09:06 +0200

    rangy

--- projet_pantenomos/imports/api/db_cases.js
+++ projet_pantenomos/imports/api/db_cases.js
@@ -4,12 +4,24 @@ import { Mongo } from 'meteor/mongo';
 //création de constantes qui serviront à référencer les BD dans le code pour la partie I
 export const Cases = new Mongo.Collection('cases');
 
+
+
 Meteor.methods({
     
     'cases.create'(case) {
 
-        let e = Cases.insert({case});
-
-        return e;
+        return Cases.insert({case});
     }
+    
 })
+
+// Meteor.methods({
+    
+//     'cases.create'(case) {
+
+//         let e = Cases.insert({case});
+
+
+//         return Cases.insert({project});
+//     }
+// })
