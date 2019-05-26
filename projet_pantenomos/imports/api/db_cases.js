947ffcc6772c740ea633392694823e4889689e94
bwicht <bertil.wicht@unil.ch>
2019-05-26 18:19:32 +0200

    Votes
    cases > votes

--- projet_pantenomos/imports/api/db_cases.js
+++ projet_pantenomos/imports/api/db_cases.js
@@ -2,14 +2,14 @@
 import { Mongo } from 'meteor/mongo';
 
 //création de constantes qui serviront à référencer les BD dans le code pour la partie I
-export const Cases = new Mongo.Collection('cases');
+export const Votes = new Mongo.Collection('votes');
 
-// Meteor.methods({
+Meteor.methods({
     
-//     'cases.create'(case) {
+    'votes.create'(vote) {
 
-//         let e = Cases.insert({case});
+        let e = Votes.insert({vote});
 
-//         return e;
-//     }
-// })
+        return e;
+    },
+})
--- projet_pantenomos/imports/ui/poll.js
+++ projet_pantenomos/imports/ui/poll.js
@@ -1,6 +1,6 @@
 //importation des méthodes
 import { ReactiveVar } from 'meteor/reactive-var';
-import  { Cases } from '../api/db_cases.js';
+import  { Votes } from '../api/db_cases.js';
 // nom de la database à remplir
 import '../templates/vot_poll.html';
 
@@ -8,13 +8,15 @@ import '../templates/vot_poll.html';
 // initialisation des variables qui définiront le choix de l'utilisateur
 Template.vot_poll.onCreated(function () {
     
+    // avis est la valeur cochée par l'individu
     this.avis ="";
+    // récupère le nombre de votes total (en faire un variable globale pour tous les utilisateurs)
     this.nombreVote = new ReactiveVar(0);
-    
-    Pour =0;
-    Contre =0;
-    PourPrincipe=0;
-    ContrePrincpe=0;
+    // Sommes des différentes possibilités
+    this.Pour =new ReactiveVar(0);
+    this.Contre =new ReactiveVar(0);
+    this.PourPrincipe=new ReactiveVar(0);
+    this.ContrePrincpe=new ReactiveVar(0);
 });
 
 
@@ -24,22 +26,20 @@ Template.vot_poll.helpers({
     },
     
     
-//     choix(){
-//         return this.avis.get();
-//       },
-//       reponse(){
-//         return radios;
-//       }
+    // avis(){
+    //     return this.avis.get();
+    //   },
+
 
  });
 
-//
+// récupération du choix de l'utilisateur en fonction de la case cochée
 Template.vot_poll.events({
 
     'click #btnAddVote': function(event,instance) {
         event.preventDefault();
         instance.nombreVote.set(instance.nombreVote.get()+1);
-        Cases.insert({
+        Votes.insert({
             totalVotes : instance.nombreVote.get()
         });
         var radios = document.getElementsByName('choice');
@@ -50,7 +50,7 @@ Template.vot_poll.events({
                 // do whatever you want with the checked radio
                 const avis = radios[i].value;
                 console.log(avis);
-                Cases.insert({avis:avis});
+                Votes.insert({avis:avis});
                 
                 // let vote = {
                 //     avis: avis,
@@ -63,7 +63,7 @@ Template.vot_poll.events({
                     // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
                
 
-                // only one radio can be logically checked, don't check the rest
+                
                 break;
             }
         
