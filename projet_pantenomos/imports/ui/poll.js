3419a3b90dafba713cb27a81e03eeeb954769b60
bwicht <bertil.wicht@unil.ch>
2019-05-26 19:57:05 +0200

    variables votes

--- projet_pantenomos/imports/ui/poll.js
+++ projet_pantenomos/imports/ui/poll.js
@@ -13,10 +13,10 @@ Template.vot_poll.onCreated(function () {
     // récupère le nombre de votes total (en faire un variable globale pour tous les utilisateurs)
     this.nombreVote = new ReactiveVar(0);
     // Sommes des différentes possibilités
-    this.Pour =new ReactiveVar(0);
-    this.Contre =new ReactiveVar(0);
-    this.PourPrincipe=new ReactiveVar(0);
-    this.ContrePrincpe=new ReactiveVar(0);
+    this.nombrePour =new ReactiveVar(0);
+    this.nombreContre =new ReactiveVar(0);
+    this.nombrePourPrincipe=new ReactiveVar(0);
+    this.nombreContrePrincipe=new ReactiveVar(0);
 });
 
 
@@ -24,10 +24,22 @@ Template.vot_poll.helpers({
     nombreVote(){
             return Template.instance().nombreVote.get();
     },
+    nombrePour(){
+        return Template.instance().nombrePour.get();
+    },
+    nombreContre(){
+        return Template.instance().nombreContre.get();
+    },
+    nombrePourPrincip(){
+        return Template.instance().nombrePourPrincipe.get();
+    },
+    nombreContrePrincipe(){
+        return Template.instance().nombreContrePrincipe.get();
+    },
     
     
     // avis(){
-    //     return this.avis.get();
+    //    return this.avis.get();
     //   },
 
 
@@ -39,9 +51,9 @@ Template.vot_poll.events({
     'click #btnAddVote': function(event,instance) {
         event.preventDefault();
         instance.nombreVote.set(instance.nombreVote.get()+1);
-        Votes.insert({
-            totalVotes : instance.nombreVote.get()
-        });
+        // Votes.insert({
+        //     totalVotes : instance.nombreVote.get()
+        // });
         var radios = document.getElementsByName('choice');
 
         for (var i = 0, length = radios.length; i < length; i++) {
@@ -50,16 +62,32 @@ Template.vot_poll.events({
                 // do whatever you want with the checked radio
                 const avis = radios[i].value;
                 console.log(avis);
-                Votes.insert({avis:avis});
+                if(avis === "Pour"){
+                   Pour = +1;
+                   instance.nombrePour.set(instance.nombrePour.get()+1);
+                } else if(avis === "Contre"){
+                    Contre =+1;
+                    instance.nombreContre.set(instance.nombreContre.get()+1);
+                } else if(avis === "PourPrincipe"){
+                    PourPrincipe =+1;
+                    instance.nombrePourPrincipe.set(instance.nombrePourPrincipee.get()+1);
+                } 
                 
-                // let vote = {
-                //     avis: avis,
-                //     // ajouter pour quelle initiative il voté ?
-                //     createdAt: new Date(),
-                //     ownerId: Meteor.userId()
-                //     };
-            
-                //     Cases.insert(vote);
+                
+                else {
+                    ContrePrincipe=+1;
+                    instance.nombreContrePrincipe.set(instance.nombreContrePrincipe.get()+1);
+
+                }
+                let vote = {
+                         avis: avis,
+                    //     // ajouter pour quelle initiative il voté ?
+                        // createdAt: new Date(),
+                        // ownerId: Meteor.userId()
+                         };
+                // Votes.insert({vote});
+                
+                
                     // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
                
 
