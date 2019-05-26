5cfeece6c1cc4b0ec68d14cc2b5ca437de5ddc62
bwicht <bertil.wicht@unil.ch>
2019-05-26 12:08:01 +0200

    VoteDatabase
    Ajout de  l'avis de l'individu dans la base de données + création de la variable vote global comptabilisant le nombre de votes totaux.

--- projet_pantenomos/imports/api/db_cases.js
+++ projet_pantenomos/imports/api/db_cases.js
@@ -3,3 +3,13 @@ import { Mongo } from 'meteor/mongo';
 
 //création de constantes qui serviront à référencer les BD dans le code pour la partie I
 export const Cases = new Mongo.Collection('cases');
+
+Meteor.methods({
+    
+    'cases.create'(case) {
+
+        let e = Cases.insert({case});
+
+        return e;
+    }
+})
--- projet_pantenomos/imports/ui/poll.js
+++ projet_pantenomos/imports/ui/poll.js
@@ -8,6 +8,9 @@ import '../templates/vot_poll.html';
 // initialisation des variables qui définiront le choix de l'utilisateur
 Template.vot_poll.onCreated(function () {
     
+    this.avis ="";
+    this.nombreVote = new ReactiveVar(0);
+    
     Pour =0;
     Contre =0;
     PourPrincipe=0;
@@ -16,51 +19,60 @@ Template.vot_poll.onCreated(function () {
 
 
 Template.vot_poll.helpers({
-    choix() {
-        return Template.instance().Pour.get()
-  
-     },
-     
-  });
+    nombreVote(){
+            return Template.instance().nombreVote.get();
+    },
+    
+    
+//     choix(){
+//         return this.avis.get();
+//       },
+//       reponse(){
+//         return radios;
+//       }
+
+ });
 
 //
 Template.vot_poll.events({
 
-    'checked .poll'(event, instance) {
-        event.target;
-        votes.update({article: 72},{$inc : {PourPrincipe : 1 }})
-    },
-
-    'checked .poll'(event, instance){
-        Cases.insert({
-            vote: choix,
-            vote: document.getElementByName('choice').value,
-            createdAt: new Date(),
-            ownerId: Meteor.userId()
-        });
-    },
-    
-
     'click #btnAddVote': function(event,instance) {
         event.preventDefault();
-        let radio = document.getElementsByName('choice').value;
-        alert(radio);
-        let recuperation = document.getElementByName('choice').value;
-        let recuperation2 = pollChoice.choice.value;
-        alert(recuperation2);
-        
-        
-        // let vote = {
-        //     vote: document.getElementByName('choice').value,
-        //     // ajouter pour quelle initiative il voté ?
-        //     createdAt: new Date(),
-        //     ownerId: Meteor.userId()
-        //     }
-    
-        //     Cases.insert(vote)
-        //     // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
-    
+        instance.nombreVote.set(instance.nombreVote.get()+1);
+        Cases.update({
+            totalVotes : instance.nombreVote.get()
+        });
+        var radios = document.getElementsByName('choice');
+
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].checked) {
+                
+                // do whatever you want with the checked radio
+                const avis = radios[i].value;
+                console.log(avis);
+                Cases.insert({avis:avis});
+                
+                // let vote = {
+                //     avis: avis,
+                //     // ajouter pour quelle initiative il voté ?
+                //     createdAt: new Date(),
+                //     ownerId: Meteor.userId()
+                //     };
             
+                //     Cases.insert(vote);
+                    // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
+               
+
+                // only one radio can be logically checked, don't check the rest
+                break;
+            }
+        
+        
+        
+        
+            // Cases.update({initativeID: _id },{$inc : {document.getElementByName('choice').value : 1 },{$inc : {VotesTotal : 1}})
+            // votes.update({article: 72},{$inc : {PourPrincipe : 1 }})
+        }    
         },
 
 });
