c15a1dcfc12949b4962a144b095ad008783a1fe3
LeaKiller <lea.keller.opp@gmail.com>
2019-05-28 15:49:24 +0200

    Création index BD surlignage (performance)

--- projet_pantenomos/server/main.js
+++ projet_pantenomos/server/main.js
@@ -6,18 +6,13 @@ import { Projects } from '../imports/api/db_projects.js';
 import { Highlightments } from '../imports/api/db_highlightments';
 import '../both';
 import { Votes } from '../imports/api/db_cases.js';
-//exécute le code au démarrage du serveur
-Meteor.startup(() => {
-
-  //NE PAS ENLEVER DES COMMENTAIRES — vide toute la base de données pour les projets /!\
-  //Highlightments.remove({});
-
-});
 
 //exécute le code au démarrage du serveur
 Meteor.startup(() => {
 
-  //NE PAS ENLEVER DES COMMENTAIRES — vide toute la base de données pour les surlignages /!\
-  //Highlightments.remove({});
+	//NE PAS ENLEVER DES COMMENTAIRES — vide toute la base de données pour les projets /!\
+	//Highlightments.remove({});
+
+	Highlightments.rawCollection().createIndex({"highlightment.project_id": 1, "highlightment.parent_id": 1, "highlightment.position": 1, "highlightment.score": 1});
 
 });
